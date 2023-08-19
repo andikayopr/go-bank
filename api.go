83d061ecea778e6d62772593b77ebc6cd34ef4af
Andika Yopi Pr <ayprihartomo@gmail.com>
2023-08-20 00:11:15 +0700

    Add create and validate JWT

--- api.go
+++ api.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
+	jwt "github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/mux"
 )
 
@@ -14,6 +16,69 @@ func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(v)
+}
+
+func createJWT(account *Account) (string, error) {
+	claims := &jwt.MapClaims{
+		"expiresAt":     15000,
+		"accountNumber": account.Number,
+	}
+
+	key := []byte(os.Getenv("JWT_SECRET"))
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, *claims)
+
+	return t.SignedString(key)
+}
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50TnVtYmVyIjo3Mjc4ODcsImV4cGlyZXNBdCI6MTUwMDB9.5n8XVCgcC1O5-eaEc6L67gD_lj4yp5N_zkE8laTqBig
+
+func permissionNotAuthorized(w http.ResponseWriter) {
+	WriteJSON(w, http.StatusForbidden, ApiError{Error: "not authorized"})
+}
+
+func withJWTAuth(handlerFunc http.HandlerFunc, s Storage) http.HandlerFunc {
+	log.Println("JWT Middleware")
+	return func(w http.ResponseWriter, r *http.Request) {
+		authToken := r.Header.Get("x-jwt-token")
+		token, err := validateJWT(authToken)
+		if err != nil {
+			permissionNotAuthorized(w)
+			return
+		}
+
+		if !token.Valid {
+			permissionNotAuthorized(w)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		log.Println(claims, "claim")
+
+		userID, err := getID(r)
+		if err != nil {
+			permissionNotAuthorized(w)
+			return
+		}
+
+		account, err := s.GetAccountByID(userID)
+
+		if account.Number != int64(claims["accountNumber"].(float64)) {
+			permissionNotAuthorized(w)
+			return
+		}
+
+		handlerFunc(w, r)
+	}
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
 }
 
 type apiFunc func(http.ResponseWriter, *http.Request) error
@@ -46,7 +111,8 @@ func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandlerFunc(s.handleAccount))
-	router.HandleFunc("/account/{id}", makeHTTPHandlerFunc(s.handleGetAccountByID))
+	router.HandleFunc("/account/{id}", withJWTAuth(makeHTTPHandlerFunc(s.handleGetAccountByID), s.store))
+	router.HandleFunc("/transfer", makeHTTPHandlerFunc(s.handleTransfer))
 	log.Println("API running on port s: ", s.listerAddr)
 
 	http.ListenAndServe(s.listerAddr, router)
@@ -106,7 +172,11 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 		return err
 	}
 
-	fmt.Printf("+v\n", account)
+	tokenString, err := createJWT(account)
+	if err != nil {
+		return err
+	}
+	fmt.Println("JWT", tokenString)
 
 	return WriteJSON(w, http.StatusOK, account)
 }
@@ -125,7 +195,13 @@ func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 }
 
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	transferReq := new(TransferRequest)
+	if err := json.NewDecoder(r.Body).Decode(transferReq); err != nil {
+		return err
+	}
+
+	defer r.Body.Close()
+	return WriteJSON(w, http.StatusOK, transferReq)
 }
 
 func getID(r *http.Request) (int, error) {
--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	store, err := NewPostgresStore()
 	if err != nil {
 		log.Fatal(err)
